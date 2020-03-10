ba9188df48ec85cdf8facbf3c8e84ead5ef2d45b
Bobby Thompson <rhthompson1986@gmail.com>
2020-03-09 19:23:05 -0700

    Initial Commit

---
+++ Gesture Homework 08Tests/Gesture_Homework_08Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Gesture_Homework_08Tests.swift
+//  Gesture Homework 08Tests
+//
+//  Created by Bobby Thompson on 3/9/20.
+//  Copyright © 2020 Bobby Thompson. All rights reserved.
+//
+
+import XCTest
+@testable import Gesture_Homework_08
+
+class Gesture_Homework_08Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
